4784814a20bc35e152989b44de07b95c2d54cfde
Wensen Ma <wensenma@Wensens-MacBook-Pro.local>
2021-02-13 01:13:56 -0800

    Update Code

--- src/main/java/com/yufeng/JxcApplication.java
+++ src/main/java/com/yufeng/JxcApplication.java
@@ -6,7 +6,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class JxcApplication {
 
-
     public static void main(String[] args) {
         SpringApplication.run(JxcApplication.class, args);
     }
